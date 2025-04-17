6644a6d004b9f5711264973e788d557b1921df5b
RicardoOliveira2025 <202503392692@alunos.estacio.br>
2025-04-17 00:40:00 +0000

    Ajustes na carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,37 +4,52 @@ int main (){
 
     printf("Cartas 1 e 2 do Super Trunfo");
 
-    //declaracao de variaveis da carta1
-    char Estado1[1];
-    char NomedaCidade1[23];
-    char CodigodaCarta1[3];
-    int Populacao1;
-    float Area1;
-    float PIB1;
-    int NumerodePontosTuristicos1;
+    //declaracao das variaveis
+    int NumerodaCarta;
+    char Estado[1];
+    char NomedaCidade[23];
+    char CodigodaCarta[3];
+    int Populacao;
+    float Area;
+    float PIB;
+    int NumerodePontosTuristicos;
 
     //Solicitacoes no monitor e saida de dados
     
-    printf("Digite o nome do Estado 1: ");
-    scanf("%s",&Estado1);
+    printf("Digite o numero da carta: ");
+    scanf("%d", &NumerodaCarta);
 
-    printf("Digite o nome da cidade 1: ");
-    scanf("%s", &NomedaCidade1);
+    printf("Digite o nome do Estado: ");
+    scanf("%s",&Estado);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &NomedaCidade);
 
     printf("Digite o codigo da carta: ");
-    scanf("%s", &CodigodaCarta1);
+    scanf("%s", &CodigodaCarta);
 
     printf("Digite a populacao: ");
-    scanf("%d", &Populacao1);
+    scanf("%d", &Populacao);
 
     printf("Digite a area da cidade em Km²: ");
-    scanf("%f", &Area1);
+    scanf("%f", &Area);
 
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &PIB1);
+    scanf("%f", &PIB);
 
     printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &NumerodePontosTuristicos1);
+    scanf("%d", &NumerodePontosTuristicos);
+
+    printf("O numero da carta eh: %d \n", NumerodaCarta);
+    printf("O nome do Estado eh: %s \n", Estado);
+    printf("O nome da cidade eh: %s \n", NomedaCidade);
+    printf("O codigo da carta eh: %s \n", CodigodaCarta);
+    printf("A populacao eh: %d \n", Populacao);
+    printf("A area eh de: %f \n", Area);
+    printf("O PIB da cidade eh de: %f \n", PIB);
+    printf("O numero de pontos turisticos eh: %d \n", NumerodePontosTuristicos);
+    
+
 
     return 0;
 }
