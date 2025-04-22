e031c1c57faf477f83f1fc5564f17f828ffc2715
RicardoOliveira2025 <202503392692@alunos.estacio.br>
2025-04-22 22:04:21 +0000

    Cartas 1 e 2 FINAL

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
 
+#include <stdio.h>
 int main (){
 
-    printf("Cartas 1 e 2 do Super Trunfo");
+    printf("Cartas 1 e 2 do Super Trunfo \n");
+
+    printf("Carta 1 \n");
 
     //declaracao das variaveis
     int NumerodaCarta;
@@ -49,7 +51,40 @@ int main (){
     printf("O PIB da cidade eh de: %f \n", PIB);
     printf("O numero de pontos turisticos eh: %d \n", NumerodePontosTuristicos);
     
+    printf("Carta 2 \n");
+   
+    printf("Digite o numero da carta: ");
+    scanf("%d", &NumerodaCarta);
 
+    printf("Digite o nome do Estado: ");
+    scanf("%s",&Estado);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &NomedaCidade);
+
+    printf("Digite o codigo da carta: ");
+    scanf("%s", &CodigodaCarta);
+
+    printf("Digite a populacao: ");
+    scanf("%d", &Populacao);
+
+    printf("Digite a area da cidade em Km²: ");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB);
+
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &NumerodePontosTuristicos);
+
+    printf("O numero da carta eh: %d \n", NumerodaCarta);
+    printf("O nome do Estado eh: %s \n", Estado);
+    printf("O nome da cidade eh: %s \n", NomedaCidade);
+    printf("O codigo da carta eh: %s \n", CodigodaCarta);
+    printf("A populacao eh: %d \n", Populacao);
+    printf("A area eh de: %f \n", Area);
+    printf("O PIB da cidade eh de: %f \n", PIB);
+    printf("O numero de pontos turisticos eh: %d \n", NumerodePontosTuristicos);
 
     return 0;
 }
